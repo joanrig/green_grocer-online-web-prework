fdd940206840368f4139d023a5300d8821bb6c95
Joan in DC <joanrigdon@gmail.com>
2019-01-01 00:47:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -76,7 +76,10 @@ end
 #{"AVOCADO" => {:price => 3.00, :clearance => true, :count => 3}}
 
 def checkout(cart, coupons)
-  cart = consolidate_cart(cart: cart)
+  cart.each do |food, food_info|
+    food_info.each do |food_name|
+  
+  cart = consolidate_cart(cart: food_name)
   cart = apply_coupons(cart: cart, coupons: coupons)
   cart = apply_clearance(cart: cart)
   binding.pry
